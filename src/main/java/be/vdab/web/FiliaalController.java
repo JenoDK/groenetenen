5ba9136d646ef0f07d6fa51dff5ba601307f5589
Jeno De Keyzer <jenodekeyzer@gmail.com>
2015-07-01 22:57:28 +0200

    TEM 18

--- src/main/java/be/vdab/web/FiliaalController.java
+++ src/main/java/be/vdab/web/FiliaalController.java
@@ -4,10 +4,14 @@ import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import be.vdab.entities.Filiaal;
+import be.vdab.exceptions.FiliaalHeeftNogWerknemersException;
 import be.vdab.services.FiliaalService;
 
 @Controller
@@ -19,6 +23,11 @@ class FiliaalController {
 	private static final Logger logger = Logger
 			.getLogger(FiliaalController.class.getName());
 	private final FiliaalService filiaalService;
+	private static final String FILIAAL_VIEW = "filialen/filiaal";
+	private static final String REDIRECT_URL_FILIAAL_NIET_GEVONDEN = "redirect:/filialen";
+	private static final String REDIRECT_URL_NA_VERWIJDEREN = "redirect:/filialen/{id}/verwijderd";
+	private static final String REDIRECT_URL_HEEFT_NOG_WERKNEMERS = "redirect:/filialen/{id}";
+	private static final String VERWIJDERD_VIEW = "filialen/verwijderd";
 
 	@Autowired
 	FiliaalController(FiliaalService filiaalService) {
@@ -43,5 +52,38 @@ class FiliaalController {
 	@RequestMapping(value = "toevoegen", method = RequestMethod.GET)
 	String createForm() {
 		return TOEVOEGEN_VIEW;
+	}
+
+	@RequestMapping(value = "{id}", method = RequestMethod.GET)
+	ModelAndView read(@PathVariable long id) {
+		ModelAndView modelAndView = new ModelAndView(FILIAAL_VIEW);
+		Filiaal filiaal = filiaalService.read(id);
+		if (filiaal != null) {
+			modelAndView.addObject(filiaal);
+		}
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "{id}/verwijderen", method = RequestMethod.POST)
+	String delete(@PathVariable long id, RedirectAttributes redirectAttributes) {
+		Filiaal filiaal = filiaalService.read(id);
+		if (filiaal == null) {
+			return REDIRECT_URL_FILIAAL_NIET_GEVONDEN;
+		}
+		try {
+			filiaalService.delete(id);
+			redirectAttributes.addAttribute("id", id).addAttribute("naam",
+					filiaal.getNaam());
+			return REDIRECT_URL_NA_VERWIJDEREN;
+		} catch (FiliaalHeeftNogWerknemersException ex) {
+			redirectAttributes.addAttribute("id", id).addAttribute("fout",
+					"Filiaal heeft nog werknemers");
+			return REDIRECT_URL_HEEFT_NOG_WERKNEMERS;
+		}
+	}
+
+	@RequestMapping(value = "{id}/verwijderd", method = RequestMethod.GET)
+	ModelAndView deleted(String naam) {
+		return new ModelAndView(VERWIJDERD_VIEW, "naam", naam);
 	}
 }
