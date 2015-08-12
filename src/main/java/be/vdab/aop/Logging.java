f534458aea2b8209d887b7d5908d1ce645df7fb4
Jeno De Keyzer <jenodekeyzer@gmail.com>
2015-08-12 18:08:08 +0200

    TEM end of course

--- src/main/java/be/vdab/aop/Logging.java
+++ src/main/java/be/vdab/aop/Logging.java
@@ -1,26 +1,24 @@
 package be.vdab.aop;
 
-import java.util.Collection;
 import java.util.Date;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-@Order(2)
-class Auditing {
-	private final static Logger logger = Logger.getLogger(Auditing.class
+class Logging {
+	private final static Logger logger = Logger.getLogger(Logging.class
 			.getName());
 
-	@AfterReturning(pointcut = "be.vdab.aop.PointcutExpressions.services()", returning = "returnValue")
-	void schrijfAudit(JoinPoint joinPoint, Object returnValue) {
+	@AfterThrowing(pointcut = "be.vdab.aop.PointcutExpressions.servicesEnTransacties()", throwing = "ex")
+	void schrijfException(JoinPoint joinPoint, Throwable ex) {
 		StringBuilder builder = new StringBuilder("\nTijdstip\t")
 				.append(new Date());
 		Authentication authentication = SecurityContextHolder.getContext()
@@ -34,15 +32,6 @@ class Auditing {
 		for (Object object : joinPoint.getArgs()) {
 			builder.append("\nParameter\t").append(object);
 		}
-		if (returnValue != null) {
-			builder.append("\nReturn\t\t");
-			if (returnValue instanceof Collection) {
-				builder.append(((Collection<?>) returnValue).size()).append(
-						" objects");
-			} else {
-				builder.append(returnValue.toString());
-			}
-		}
-		logger.info(builder.toString());
+		logger.log(Level.SEVERE, builder.toString(), ex);
 	}
 }
